d95b89b81973d007684db870fddeba6fff96eccf
Michaela Dane <michaeladane@Michaelas-MacBook-Air.local>
2026-06-24 12:57:30 -0700

    cleanup: remove debug node TCP check from wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -9,17 +9,7 @@ sed -i "s/listen 8080/listen $NGINX_PORT/" /etc/nginx/nginx.conf
 
 nginx -t 2>&1
 nginx -g "daemon off;" 2>&1 &
-NGINX_PID=$!
-sleep 2
-
-echo "[WRAPPER] nginx alive: $(kill -0 $NGINX_PID 2>&1 && echo YES || echo NO)"
-node -e "
-const net = require('net');
-const s = net.createServer();
-s.listen($NGINX_PORT, '0.0.0.0', () => { console.log('[WRAPPER] port $NGINX_PORT IS FREE - nginx NOT bound'); s.close(); });
-s.on('error', () => console.log('[WRAPPER] port $NGINX_PORT IN USE - nginx IS bound'));
-setTimeout(() => process.exit(0), 2000);
-"
+sleep 1
 
 unset PORT
 cd /app
